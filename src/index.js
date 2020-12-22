1804ea5b5b75f2f5b74ca881f49a0a8999f4fa41
h1m <himanshu.yadav643@gmail.com>
2020-12-22 23:39:13 +0530

    project finished

--- src/index.js
+++ src/index.js
@@ -1,54 +1,88 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import SeasonDisplay from "./SeasonDisplay";
+
+// // const App = () => {
+// //   window.navigator.geolocation.getCurrentPosition(
+// //     (position) => console.log(position),
+// //     (err) => console.log(err)
+// //   );
+
+// //   return <div>Hi There!</div>;
+// // };
+
+// class App extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = { lat: null, errorMessage: "" };
+//   // }
+//   state = { lat: null, errorMessage: "" };
+
+//   // componentDidMount() {
+//   //   console.log("My component was rendered to the screen");
+//   // }
+//   // componentDidUpdate() {
+//   //   console.log("My component was just updated");
+//   // }
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         this.setState({ lat: position.coords.latitude });
+//       },
+//       (err) => {
+//         this.setState({ errorMessage: err.message });
+//       }
+//     );
+//   }
+
+//   render() {
+//     // return (
+//     //   <div>
+//     //     Latitude: {this.state.lat} <br />
+//     //     Erorr: {this.state.errorMessage}
+//     //   </div>
+//     // );
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     }
+//     if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay Lat={this.state.lat} />;
+//     }
+//     return <div>Loading!</div>;
+//   }
+// }
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//     (position) => console.log(position),
-//     (err) => console.log(err)
-//   );
-
-//   return <div>Hi There!</div>;
-// };
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { lat: null, errorMessage: "" };
-  // }
   state = { lat: null, errorMessage: "" };
 
-  // componentDidMount() {
-  //   console.log("My component was rendered to the screen");
-  // }
-  // componentDidUpdate() {
-  //   console.log("My component was just updated");
-  // }
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
-    // return (
-    //   <div>
-    //     Latitude: {this.state.lat} <br />
-    //     Erorr: {this.state.errorMessage}
-    //   </div>
-    // );
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
+
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay Lat={this.state.lat} />;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading!</div>;
+
+    return <Spinner message="Please accept location request!" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
